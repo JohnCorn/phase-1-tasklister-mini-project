5614325c6b5fbbb7e053a68772bd0bf1d12fbc72
John Corn <jcornacchioli@gmail.com>
2023-06-11 12:25:44 -0400

    lab complete

--- src/index.js
+++ src/index.js
@@ -1,3 +1,45 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  document.querySelector('form').addEventListener('submit', (e) =>{
+    AddTODO(e);
+  } );
 });
+
+
+function AddTODO(e)
+{
+  e.preventDefault();
+
+
+  let textValue = document.getElementById('new-task-description').value;
+
+
+  if (textValue == "")
+    return;
+
+
+  let p = document.createElement('p');
+  p.textContent = textValue;
+
+
+  let btn = document.createElement('button');
+  btn.textContent = 'x';
+  btn.addEventListener('click', () =>{
+    RemoveTODO(p);
+  });
+
+
+  p.appendChild(btn);
+
+
+  document.querySelector('#list').appendChild(p);
+
+
+  // Clear submit
+  document.getElementById('new-task-description').value = "";
+}
+
+
+function RemoveTODO(p)
+{
+  p.remove();
+}
